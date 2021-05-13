4294cddb74af7af583266383fca415beae234c09
Viktor Török <torokv@vnet.hu>
2021-05-13 13:48:27 +0200

    Creating Example1

--- RegexTest01/Program.cs
+++ RegexTest01/Program.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RegexTest01
 {
     class Program
     {
+        static void Example1()
+        {
+            string text = "pista.bacsi@lidercfeny.hu";
+            string pattern = @"@.+.\.hu$";
+
+            Match match = Regex.Match(text, pattern);
+
+            if (match.Success)
+            {
+                Console.WriteLine($"Index:    {match.Index}");
+                Console.WriteLine($"Length:   {match.Length}");
+                Console.WriteLine($"Value:    {match.Value}");
+                Console.WriteLine($"ToString: {match.ToString()}");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Example 1");
+            Example1();
+            Console.WriteLine();
         }
     }
 }
