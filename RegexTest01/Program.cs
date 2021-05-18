986fdec3eccc2ac402fdc88e8d66b0777be99146
Viktor Török <torokv@vnet.hu>
2021-05-18 12:51:13 +0200

    Creating Example 2-5

--- RegexTest01/Program.cs
+++ RegexTest01/Program.cs
@@ -9,7 +9,7 @@ namespace RegexTest01
 {
     class Program
     {
-        static void Example1()
+        private static void Example1()
         {
             string text = "pista.bacsi@lidercfeny.hu";
             string pattern = @"@.+.\.hu$";
@@ -25,10 +25,67 @@ namespace RegexTest01
             }
         }
 
+        private static void Example2()
+        {
+            string text = "pista.bacsi@lidercfeny.hu";
+            string pattern = @"@.+.\.hu$";
+
+            if (Regex.IsMatch(text, pattern))
+            {
+                Console.WriteLine("Match found");
+            }
+        }
+
+        private static void Example3()
+        {
+            string text = "pista.bacsi@lidercfeny.hu;feri.bacsi@zombiapokalipszis.hu";
+            string pattern = @"@[a-z]+\.hu";
+
+            foreach (var match in Regex.Matches(text, pattern))
+            {
+                Console.WriteLine(match);
+            }
+        }
+
+        private static void Example4()
+        {
+            string text = "abc12345x";
+            string pattern = @"\d+";
+
+            Regex regex = new Regex(pattern);
+
+            Console.WriteLine(regex.IsMatch(text));
+            Console.WriteLine(regex.Match(text));
+        }
+
+        private static void Example5()
+        {
+            string text = "The Good Old PISTA";
+            string pattern = @"Old\sp\w{1,3}a";
+
+            Console.WriteLine(Regex.Match(text, pattern, RegexOptions.IgnoreCase));
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Example 1");
             Example1();
+            Console.WriteLine();
+
+            Console.WriteLine("Example 2");
+            Example2();
+            Console.WriteLine();
+
+            Console.WriteLine("Example 3");
+            Example3();
+            Console.WriteLine();
+
+            Console.WriteLine("Example 4");
+            Example4();
+            Console.WriteLine();
+
+            Console.WriteLine("Example 5");
+            Example5();
             Console.WriteLine();
         }
     }
